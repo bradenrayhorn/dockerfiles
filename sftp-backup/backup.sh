26bfcba4d2b0c092f91767fbf20772eafe4118ff
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2025-06-28 08:21:45 -0500

    sftp-backup: improve archive folder structure

--- sftp-backup/backup.sh
+++ sftp-backup/backup.sh
@@ -8,7 +8,9 @@ log() {
 
 # create working directories
 backup_dir="$(mktemp -d)"
-cd $backup_dir
+backup_dir_folder=$backup_dir/archive
+mkdir $backup_dir_folder
+working_dir="$(mktemp -d)"
 
 # setup auth
 keyfile="$(mktemp -d)/key"
@@ -18,13 +20,14 @@ chmod 600 $keyfile
 mkdir -p ~/.ssh
 ssh-keyscan -p $SFTP_PORT -H $SFTP_HOST >> ~/.ssh/known_hosts
 
-# copy all files to backup dir
-scp -i $keyfile -P $SFTP_PORT -r $SFTP_USERNAME@$SFTP_HOST:/ $backup_dir
+# copy all files to backup folder
+scp -i $keyfile -P $SFTP_PORT -r $SFTP_USERNAME@$SFTP_HOST:/ $backup_dir_folder
 
 # compress and upload
 log "Compressing..."
-tar -cf - $backup_dir | xz -9 > archive.tar.xz
+cd $backup_dir
+tar -cf - $backup_dir_folder | xz -9 > $working_dir/archive.tar.xz
 
 log "Backing up with marmalade"
-marmalade backup -f archive.tar.xz
+marmalade backup -f $working_dir/archive.tar.xz
 log "Backup process completed successfully!"
